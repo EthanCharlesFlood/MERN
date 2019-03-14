9ae5ccfca5371a65b1e6afbf9183edc23c272cfe
Ethan Flood <ethan.flood@gmail.com>
2019-03-14 18:27:07 -0400

    fix typo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (res,req) => res.send("HELLO WORLD"));
+app.get("/", (req,res) => res.send("HELLO WORLD"));
 
 const port = process.env.PORT || 5000;
 
