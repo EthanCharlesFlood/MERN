6cf07768843e7216427a2edc641d6264904eb55e
Ethan Flood <ethan.flood@gmail.com>
2019-03-14 18:55:31 -0400

    more setup

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require('mongoose');
+
 const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
 app.get("/", (req,res) => res.send("HELLO WORLD"));
 
