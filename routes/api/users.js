f6fb1df4170a9bd3ab462c0353423438c8f65fe9
Ethan Flood <ethan.flood@gmail.com>
2019-03-18 10:59:31 -0400

    add new user functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,3 +3,28 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 
 const router = express.Router();
+
+
+router.post('/register', (req,res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({email: "A user has already registered this email"});
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                });
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                });
+            }
+        })
+})
