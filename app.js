c8f8e970299d46e334fdb30cd8f0074b4077b367
Ethan Flood <ethan.flood@gmail.com>
2019-03-20 16:01:52 -0400

    update from deprecated string parser

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose
-    .connect(db)
+    .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
